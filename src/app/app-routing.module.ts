f2d124b99e39624079ddb6feaa50079edc20a2c6
nexus122 <juan-pablo122@hotmail.es>
2020-12-30 16:00:46 +0100

    Select de paises añadido

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,8 +11,8 @@ const routes: Routes = [
   {path: 'home/:id', component: HomeCountryComponent}, // Ruta para el componente home-
   {path: 'search', component: SearchComponent}, // Ruta para el componente buscador.
   {path: 'artist/:id', component: ArtistaComponent}, // Ruta para el componente artista, se envia tambien una id como parametro para saber que artista cargar
-  {path: '', pathMatch: 'full', redirectTo: 'home'}, // Ruta que lleva al componente home por defecto si no coincide con ninguna anterior
-  {path: '**', pathMatch: 'full', redirectTo: 'home'} // Ruta que lleva al componente home si no coincide con ninguna anterior
+  {path: '', pathMatch: 'full', redirectTo: 'home/es'}, // Ruta que lleva al componente home por defecto si no coincide con ninguna anterior
+  {path: '**', pathMatch: 'full', redirectTo: 'home/es'} // Ruta que lleva al componente home si no coincide con ninguna anterior
 ];
 
 @NgModule({
--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,15 +1,124 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styles: []
+  styles: ["./navbar.component.css"]
 })
 export class NavbarComponent implements OnInit {
+  paises: any[] = []
 
-  constructor() { }
+  constructor(private router: Router) {
+    this.paises = [[ "AD", "Andorra"],
+    [ "AE", "United Arab Emirates"],    
+    [ "AL", "Albania"],
+    [ "AR", "Argentina"],
+    [ "AT", "Austria"],
+    [ "AU", "Australia"],
+    //B
+    [ "BA", "Bosnia and Herzegovina"],
+    [ "BE", "Belgium"],
+    [ "BG", "Bulgaria"],
+    [ "BH", "Bahrain"],
+    [ "BO", "Bolivia, Plurinational State of"],
+    [ "BR", "Brazil"],
+    [ "BY", "Belarus"],
+    //C
+    [ "CA", "Canada"],
+    [ "CH", "Switzerland"],
+    [ "CL", "Chile"],
+    [ "CO", "Colombia"],
+    [ "CR", "Costa Rica"],
+    [ "CY", "Cyprus"],
+    [ "CZ", "Czech Republic"],
+    //D
+    [ "DE", "Germany"],
+    [ "DK", "Denmark"],
+    [ "DO", "Dominican Republic"],
+    [ "DZ", "Algeria"],
+    //E
+    [ "EC", "Ecuador"],
+    [ "EE", "Estonia"],
+    [ "EG", "Egypt"],
+    [ "ES", "Spain"],
+    //F
+    [ "FI", "Finland"],
+    [ "FR", "France"],
+    //G
+    [ "GB", "United Kingdom"],
+    [ "GR", "Greece"],
+    [ "GT", "Guatemala"],
+    //H
+    [ "HK", "Hong Kong"],
+    [ "HN", "Honduras"],
+    [ "HR", "Croatia"],
+    [ "HU", "Hungary"],
+    //I
+    [ "ID", "Indonesia"],
+    [ "IE", "Ireland"],
+    [ "IL", "Israel"],
+    [ "IN", "India"],
+    [ "IS", "Iceland"],
+    [ "IT", "Italy"],
+    //J
+    [ "JP", "Japan"],
+    //K
+    [ "KW", "Kuwait"],
+    //L
+    [ "LB", "Lebanon"],
+    [ "LI", "Liechtenstein"],
+    [ "LU", "Luxembourg"],
+    [ "LV", "Latvia"],
+    //M
+    [ "MA", "Morocco"],
+    [ "MO", "Macao"],
+    [ "MT", "Malta"],
+    [ "MX", "Mexico"],
+    [ "MY", "Malaysia"],
+    //N
+    [ "NI", "Nicaragua"],
+    [ "NL", "Netherlands"],
+    [ "NO", "Norway"],
+    [ "NZ", "New Zealand"],
+    //O
+    [ "OM", "Oman"],
+    //P
+    [ "PA", "Panama"],
+    [ "PE", "Peru"],
+    [ "PH", "Philippines"],
+    [ "PL", "Poland"],
+    [ "PS", "Palestine, State of"],
+    [ "PT", "Portugal"],
+    [ "PY", "Paraguay"],
+    //Q
+    [ "QA", "Qatar"],
+    //R
+    [ "RO", "Romania"],
+    [ "RS", "Serbia"],
+    [ "RU", "Russian Federation"],
+    //S
+    [ "SA", "Saudi Arabia"],
+    [ "SE", "Sweden"],
+    [ "SK", "Slovakia"],
+    //T
+    [ "TH", "Thailand"],
+    [ "TN", "Tunisia"],
+    [ "TR", "Turkey"],
+    //U
+    [ "UA", "Ukraine"],
+    [ "US", "United States of America"],
+    [ "UY", "Uruguay"],
+    //V
+    [ "VN", "Viet Nam"],
+
+    //Z
+    [ "ZA", "South Africa"],
+    ]
+  }
 
   ngOnInit() {
   }
-
+  
+  onNavigate(location: string){ this.router.navigateByUrl("home/"+location);; }
 }
